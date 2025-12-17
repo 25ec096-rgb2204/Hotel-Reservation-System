78529f873fcbd0d14b5fa949aac49de6e5adb551
25ec096-rgb2204 <25ec096@rmkec.ac.in>
2025-12-17 17:47:27 +0530

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,10 @@
+public class Room {
+    int roomNumber;
+    String type;
+    boolean isAvailable = true;
+
+    public Room(int roomNumber, String type) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+    }
+}
