190464703a9a8b6e373f9e399ee440eff00ee5d2
25ec096-rgb2204 <25ec096@rmkec.ac.in>
2025-12-17 17:48:22 +0530

    Create Booking.java

---
+++ Booking.java
@@ -0,0 +1,9 @@
+public class Booking {
+    String customerName;
+    Room room;
+
+    public Booking(String customerName, Room room) {
+        this.customerName = customerName;
+        this.room = room;
+    }
+}
